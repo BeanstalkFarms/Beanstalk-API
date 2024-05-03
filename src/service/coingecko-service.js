b0b315d9d41623253887da1470eb071f6a681a22
Soil King <157099073+soilking@users.noreply.github.com>
2024-05-03 14:44:22 -0700

    services in static classes

--- src/service/coingecko-service.js
+++ src/service/coingecko-service.js
@@ -3,49 +3,50 @@ const { basinSG, gql } = require("../datasources/subgraph-client");
 const { getConstantProductPrice } = require("../utils/constant-product");
 const BlockUtil = require("../utils/block");
 
-async function getTickers(options = {}) {
-  // Determine block
-  const block = await BlockUtil.blockForSubgraphFromOptions(basinSG, options);
+class CoingeckoService {
 
-  // Retrieve results from Basin subgraph
-  const result = await basinSG(gql`
-    {
-      wells(block: {number: ${block.number}}) {
-        id
-        tokens {
+  static async getTickers(options = {}) {
+    // Determine block
+    const block = await BlockUtil.blockForSubgraphFromOptions(basinSG, options);
+  
+    // Retrieve results from Basin subgraph
+    const result = await basinSG(gql`
+      {
+        wells(block: {number: ${block.number}}) {
           id
-          decimals
+          tokens {
+            id
+            decimals
+          }
+          reserves
         }
-        reserves
-      }
-    }`
-  );
-
-  const allTickers = [];
-
-  // For each well in the subgraph, construct a formatted response
-  for (const well of result.wells) {
-    const token0 = well.tokens[0].id;
-    const token1 = well.tokens[1].id;
-    const price = getConstantProductPrice(well.reserves.map(BigNumber.from), well.tokens.map(t => t.decimals));
-    const ticker = {
-      ticker_id: `${token0}_${token1}`,
-      base_currency: token0,
-      target_currency: token1,
-      pool_id: well.id,
-      last_price: price.string[0],
-      base_volume: null,
-      target_volume: null,
-      liquidity_in_usd: null,
-      high: null,
-      low: null
-    };
-
-    allTickers.push(ticker);
+      }`
+    );
+  
+    const allTickers = [];
+  
+    // For each well in the subgraph, construct a formatted response
+    for (const well of result.wells) {
+      const token0 = well.tokens[0].id;
+      const token1 = well.tokens[1].id;
+      const price = getConstantProductPrice(well.reserves.map(BigNumber.from), well.tokens.map(t => t.decimals));
+      const ticker = {
+        ticker_id: `${token0}_${token1}`,
+        base_currency: token0,
+        target_currency: token1,
+        pool_id: well.id,
+        last_price: price.string[0],
+        base_volume: null,
+        target_volume: null,
+        liquidity_in_usd: null,
+        high: null,
+        low: null
+      };
+  
+      allTickers.push(ticker);
+    }
+    return allTickers;
   }
-  return allTickers;
 }
 
-module.exports = {
-  getTickers
-}
+module.exports = CoingeckoService;
--- src/service/price-service.js
+++ src/service/price-service.js
@@ -3,39 +3,39 @@ const { asyncPriceV1ContractGetter, asyncUsdOracleContractGetter } = require("..
 const BlockUtil = require("../utils/block");
 const { createNumberSpread } = require("../utils/number");
 
-async function getBeanPrice(options = {}) {
-  const block = await BlockUtil.blockFromOptions(options);
-  const priceContract = await asyncPriceV1ContractGetter();
-  const priceResult = await priceContract.callStatic.price({ blockTag: block.number });
+class PriceService {
 
-  // Convert from hex to a readable format. For now the pool prices are omitted
-  const readable = {
-    block: block.number,
-    timestamp: block.timestamp,
-    price: createNumberSpread(priceResult.price, 6, 4).float,
-    liquidityUSD: createNumberSpread(priceResult.liquidity, 6, 2).float,
-    deltaB: createNumberSpread(priceResult.deltaB, 6, 0).float,
-  };
-  return readable;
-}
-
-async function getTokenPrice(token, options = {}) {
-  const block = await BlockUtil.blockFromOptions(options);
-  const usdOracle = await asyncUsdOracleContractGetter();
-  const result = await usdOracle.callStatic.getUsdPrice(token, { blockTag: block.number });
-  // getUsdPrice returns a twa price, but with no lookback. Its already instantaneous but needs conversion
-  const instPrice = BigNumber.from(10).pow(24).div(result);
+  static async getBeanPrice(options = {}) {
+    const block = await BlockUtil.blockFromOptions(options);
+    const priceContract = await asyncPriceV1ContractGetter();
+    const priceResult = await priceContract.callStatic.price({ blockTag: block.number });
   
-  const readable = {
-    block: block.number,
-    timestamp: block.timestamp,
-    token,
-    usdPrice: createNumberSpread(instPrice, 6, 2).float
-  };
-  return readable;
+    // Convert from hex to a readable format. For now the pool prices are omitted
+    const readable = {
+      block: block.number,
+      timestamp: block.timestamp,
+      price: createNumberSpread(priceResult.price, 6, 4).float,
+      liquidityUSD: createNumberSpread(priceResult.liquidity, 6, 2).float,
+      deltaB: createNumberSpread(priceResult.deltaB, 6, 0).float,
+    };
+    return readable;
+  }
+  
+  static async getTokenPrice(token, options = {}) {
+    const block = await BlockUtil.blockFromOptions(options);
+    const usdOracle = await asyncUsdOracleContractGetter();
+    const result = await usdOracle.callStatic.getUsdPrice(token, { blockTag: block.number });
+    // getUsdPrice returns a twa price, but with no lookback. Its already instantaneous but needs conversion
+    const instPrice = BigNumber.from(10).pow(24).div(result);
+    
+    const readable = {
+      block: block.number,
+      timestamp: block.timestamp,
+      token,
+      usdPrice: createNumberSpread(instPrice, 6, 2).float
+    };
+    return readable;
+  }
 }
 
-module.exports = {
-  getBeanPrice,
-  getTokenPrice
-}
+module.exports = PriceService;
