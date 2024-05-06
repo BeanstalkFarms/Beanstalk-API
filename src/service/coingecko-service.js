ebbaf74847fb8cb6b4f9d8c1f84807f6de6123cf
Soil King <157099073+soilking@users.noreply.github.com>
2024-05-06 12:02:50 -0700

    generic subgraph paginate all

--- src/service/coingecko-service.js
+++ src/service/coingecko-service.js
@@ -3,6 +3,9 @@ const { basinSG, gql } = require("../datasources/subgraph-client");
 const { getConstantProductPrice } = require("../utils/pool/constant-product");
 const BlockUtil = require("../utils/block");
 const { calcPoolLiquidityUSD } = require("../utils/pool/liquidity");
+const subgraphClient = require("../datasources/subgraph-client");
+
+const ONE_DAY = 60 * 60 * 24;
 
 class CoingeckoService {
 
@@ -34,7 +37,7 @@ class CoingeckoService {
 
       const reservesBN = well.reserves.map(BigNumber.from);
       const poolPrice = getConstantProductPrice(reservesBN, well.tokens.map(t => t.decimals));
-      const poolLiquidity = await calcPoolLiquidityUSD(well.tokens, reservesBN);
+      const poolLiquidity = await calcPoolLiquidityUSD(well.tokens, reservesBN, block.number);
       
       const ticker = {
         ticker_id: `${token0}_${token1}`,
@@ -53,6 +56,71 @@ class CoingeckoService {
     }
     return allTickers;
   }
+
+  static async getWellVolume(wellAddress, timestamp, lookback = ONE_DAY) {
+
+    const sgResults = await allPaginatedSG(
+      basinSG,
+      gql`
+      {
+        swaps {
+          amountIn
+          amountOut
+          fromToken {
+            id
+            decimals
+          }
+          toToken {
+            decimals
+            id
+          }
+          timestamp
+        }
+      }`,
+      'timestamp',
+      (timestamp - lookback).toFixed(0),
+      'asc'
+    );
+    console.log(sgResults);
+  }
+}
+
+/**
+ * Paginates a given subgraph query according to Graph Protocol's GraphQL API spec.
+ * Only a single entity type should be requested at a time.
+ * 
+ * @param {function} subgraphClient 
+ * @param {string} query - the query to be paginated
+ * @param {string} paginateField - the field to paginate on
+ * @param {string} firstValue - the initial value to begin with of the paginateField
+ * @param {'asc' | 'desc'} paginateDirection 
+ * 
+ * TODO: need a terminating condition if desc direction is used
+ * 
+ * @returns all results matching the query
+ */
+async function allPaginatedSG(subgraphClient, query, paginateField, firstValue, paginateDirection) {
+
+  const PAGE_SIZE = 1000;
+  const whereSuffix = paginateDirection === 'asc' ? '_gt' : '_lt';
+
+  const retval = [];
+  while (firstValue) {
+    // Construct arguments for pagination
+    const paginateArguments = `(where: {${paginateField}${whereSuffix}: "${firstValue}"}, first: ${PAGE_SIZE}, orderBy: ${paginateField}, orderDirection: ${paginateDirection})`
+    let entityName = '';
+    // Add the generated arguments to the query
+    const paginatedQuery = query.replace(/(\w+)\s{/, (match, p1) => {
+      entityName = p1;
+      return `${entityName} ${paginateArguments} {`;
+    });
+    const result = await subgraphClient(paginatedQuery);
+
+    // Record the results and repeat as necessary
+    retval.push(...result[entityName]);
+    firstValue = result[entityName][PAGE_SIZE - 1]?.[paginateField];
+  }
+  return retval;
 }
 
 module.exports = CoingeckoService;
--- tests/coingecko-service.test.js
+++ tests/coingecko-service.test.js
@@ -1,19 +1,16 @@
-jest.mock("../src/datasources/subgraph-client", () => ({
-  ...jest.requireActual("../src/datasources/subgraph-client"),
-  basinSG: jest.fn()
-}));
-const { basinSG } = require("../src/datasources/subgraph-client");
+const SubgraphClients = require("../src/datasources/subgraph-client");
 
 const BlockUtil = require("../src/utils/block");
 jest.spyOn(BlockUtil, 'blockForSubgraphFromOptions').mockResolvedValue({ number: 19000000, timestamp: 1705173443 });
 
-const { getTickers } = require("../src/service/coingecko-service");
+const { getTickers, getWellVolume } = require("../src/service/coingecko-service");
+const { BEANWETH } = require("../src/constants/addresses");
 
 describe('CoingeckoService', () => {
 
   it('should return all Basin tickers in the expected format', async () => {
     const wellsResponse = JSON.parse('{"wells":[{"id":"0xbea0e11282e2bb5893bece110cf199501e872bad","tokens":[{"id":"0xbea0000029ad1c77d3d5d23ba2d8893db9d1efab","decimals":6},{"id":"0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2","decimals":18}],"reserves":["13776890377201","5362472327552046319200"]}]}');
-    basinSG.mockResolvedValue(wellsResponse);
+    jest.spyOn(SubgraphClients, 'basinSG').mockResolvedValue(wellsResponse);
 
     const tickers = await getTickers({ blockNumber: 19000000 });
     expect(tickers).toHaveLength(1);
@@ -24,10 +21,14 @@ describe('CoingeckoService', () => {
     expect(tickers[0].last_price).toEqual('0.000389236771196659');
     // expect(tickers[0].base_volume).toEqual('');
     // expect(tickers[0].target_volume).toEqual('');
-    expect(tickers[0].liquidity_in_usd).toEqual('30488648');
+    expect(tickers[0].liquidity_in_usd).toEqual('27491580');
     // expect(tickers[0].high).toEqual('');
     // expect(tickers[0].low).toEqual('');
 
     // console.log(tickers);
+  });
+
+  it('should calculate token volume in the well', async () => {
+    const volume = await getWellVolume(BEANWETH, 1715020584);
   });
 });
