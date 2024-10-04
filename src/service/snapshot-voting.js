e72f7092dc624eb89f11439680e4c8b90c33d7ce
Soil King <157099073+soilking@users.noreply.github.com>
2024-10-04 11:27:56 -0700

    accept eth block number for voting power

--- src/service/snapshot-voting.js
+++ src/service/snapshot-voting.js
@@ -17,17 +17,19 @@ class SnapshotVotingService {
       functions.push(async () => {
         // Assumption is that arb is the active chain - switch the undefined/blockNumber to change this.
         const [ethDelegators, arbDelegators] = await Promise.all([
-          SnapshotSubgraphRepository.getDelegations(address, 'eth', undefined),
-          SnapshotSubgraphRepository.getDelegations(address, 'arb', blockNumber)
+          SnapshotSubgraphRepository.getDelegations(address, 'eth', blockNumber),
+          SnapshotSubgraphRepository.getDelegations(address, 'arb', undefined)
         ]);
         voterAccounts[address] = [...new Set([address, ...ethDelegators, ...arbDelegators])];
         allRelevantAccounts.push(...voterAccounts[address]);
       });
     }
     await PromiseUtil.runBatchPromises(functions, 20);
+    console.log(voterAccounts);
 
     // Get stalk balance of all relevant accounts
     const stalkBalances = await SnapshotVotingService._getStalkBalances(allRelevantAccounts, blockNumber);
+    console.log(stalkBalances);
 
     for (const address of addresses) {
       let votingPower = voterAccounts[address].reduce((acc, next) => {
