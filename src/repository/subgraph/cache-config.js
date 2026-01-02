b20cda2f5f20fc18f24d4643a287ff95762c9a5d
Soil King <157099073+soilking@users.noreply.github.com>
2026-01-02 11:00:24 -0800

    Update cache config

--- src/repository/subgraph/cache-config.js
+++ src/repository/subgraph/cache-config.js
@@ -10,124 +10,67 @@ const paginationSettings = (fieldName, { objectField, objectAccessor, orderBy } 
 
 // Must be List queries that dont require explicitly provided id (in subgraph framework, usually ending in 's')
 const SG_CACHE_CONFIG = {
-  //////// PINTOSTALK SUBGRAPH /////////
+  //////// BEANSTALK SUBGRAPH /////////
   cache_siloHourlySnapshots: {
-    subgraph: 'pintostalk',
+    subgraph: 'beanstalk',
     queryName: 'siloHourlySnapshots',
     client: (c) => c.SG.BEANSTALK,
     paginationSettings: paginationSettings('season'),
     omitFields: ['silo']
   },
   cache_fieldHourlySnapshots: {
-    subgraph: 'pintostalk',
+    subgraph: 'beanstalk',
     queryName: 'fieldHourlySnapshots',
     client: (c) => c.SG.BEANSTALK,
     paginationSettings: paginationSettings('season'),
     omitFields: ['field']
   },
-  cache_gaugesInfoHourlySnapshots: {
-    subgraph: 'pintostalk',
-    queryName: 'gaugesInfoHourlySnapshots',
-    client: (c) => c.SG.BEANSTALK,
-    paginationSettings: paginationSettings('season'),
-    omitFields: ['gaugesInfo']
-  },
-  cache_marketPerformanceSeasonals: {
-    subgraph: 'pintostalk',
-    queryName: 'marketPerformanceSeasonals',
-    client: (c) => c.SG.BEANSTALK,
-    paginationSettings: paginationSettings('season'),
-    omitFields: ['silo']
-  },
   cache_podMarketplaceHourlySnapshots: {
-    subgraph: 'pintostalk',
+    subgraph: 'beanstalk',
     queryName: 'podMarketplaceHourlySnapshots',
     client: (c) => c.SG.BEANSTALK,
     paginationSettings: paginationSettings('season'),
     omitFields: ['podMarketplace']
   },
   cache_seasons: {
-    subgraph: 'pintostalk',
+    subgraph: 'beanstalk',
     queryName: 'seasons',
     client: (c) => c.SG.BEANSTALK,
     paginationSettings: paginationSettings('season'),
     omitFields: ['beanstalk']
   },
   cache_siloAssetHourlySnapshots: {
-    subgraph: 'pintostalk',
+    subgraph: 'beanstalk',
     queryName: 'siloAssetHourlySnapshots',
     client: (c) => c.SG.BEANSTALK,
     paginationSettings: paginationSettings('season'),
     omitFields: ['siloAsset']
   },
-  cache_tractorHourlySnapshots: {
-    subgraph: 'pintostalk',
-    queryName: 'tractorHourlySnapshots',
-    client: (c) => c.SG.BEANSTALK,
-    paginationSettings: paginationSettings('season'),
-    omitFields: ['tractor']
-  },
   cache_unripeTokenHourlySnapshots: {
-    subgraph: 'pintostalk',
+    subgraph: 'beanstalk',
     queryName: 'unripeTokenHourlySnapshots',
     client: (c) => c.SG.BEANSTALK,
     paginationSettings: paginationSettings('season'),
     omitFields: ['underlyingToken', 'unripeToken']
   },
   cache_whitelistTokenHourlySnapshots: {
-    subgraph: 'pintostalk',
+    subgraph: 'beanstalk',
     queryName: 'whitelistTokenHourlySnapshots',
     client: (c) => c.SG.BEANSTALK,
     paginationSettings: paginationSettings('season'),
     omitFields: ['token']
   },
-  cache_wrappedDepositERC20HourlySnapshots: {
-    subgraph: 'pintostalk',
-    queryName: 'wrappedDepositERC20HourlySnapshots',
-    client: (c) => c.SG.BEANSTALK,
-    paginationSettings: paginationSettings('season'),
-    omitFields: ['siloHourlySnapshot', 'token']
-  },
-  //////// PINTO SUBGRAPH /////////
+  //////// BEAN SUBGRAPH /////////
   cache_beanHourlySnapshots: {
-    subgraph: 'pinto',
+    subgraph: 'bean',
     queryName: 'beanHourlySnapshots',
     client: (c) => c.SG.BEAN,
-    paginationSettings: paginationSettings('seasonNumber'),
-    omitFields: ['bean', 'crossEvents', 'season']
-  },
-  cache_farmerBalanceHourlySnapshots: {
-    subgraph: 'pinto',
-    queryName: 'farmerBalanceHourlySnapshots',
-    client: (c) => c.SG.BEAN,
-    paginationSettings: paginationSettings('seasonNumber'),
-    omitFields: ['farmerBalance', 'season']
-  },
-  cache_poolHourlySnapshots: {
-    subgraph: 'pinto',
-    queryName: 'poolHourlySnapshots',
-    client: (c) => c.SG.BEAN,
-    paginationSettings: paginationSettings('seasonNumber'),
-    omitFields: ['pool', 'season', 'crossEvents']
-  },
-  cache_tokenHourlySnapshots: {
-    subgraph: 'pinto',
-    queryName: 'tokenHourlySnapshots',
-    client: (c) => c.SG.BEAN,
-    paginationSettings: paginationSettings('seasonNumber'),
-    omitFields: ['token', 'season']
-  },
-  //////// EXCHANGE SUBGRAPH /////////
-  cache_beanstalkHourlySnapshots: {
-    subgraph: 'exchange',
-    queryName: 'beanstalkHourlySnapshots',
-    client: (c) => c.SG.BASIN,
     paginationSettings: paginationSettings('season_: {season', {
       objectField: 'season',
       objectAccessor: (o) => o.season.season,
       orderBy: 'season__season'
     }),
-    omitFields: ['season', 'wells'],
+    omitFields: ['bean', 'crossEvents', 'season'],
     syntheticFields: [
       {
         queryAccessor: 'season { season }',
@@ -137,16 +80,16 @@ const SG_CACHE_CONFIG = {
       }
     ]
   },
-  cache_wellHourlySnapshots: {
-    subgraph: 'exchange',
-    queryName: 'wellHourlySnapshots',
-    client: (c) => c.SG.BASIN,
+  cache_poolHourlySnapshots: {
+    subgraph: 'bean',
+    queryName: 'poolHourlySnapshots',
+    client: (c) => c.SG.BEAN,
     paginationSettings: paginationSettings('season_: {season', {
       objectField: 'season',
       objectAccessor: (o) => o.season.season,
       orderBy: 'season__season'
     }),
-    omitFields: ['season', 'well'],
+    omitFields: ['pool', 'season', 'crossEvents'],
     syntheticFields: [
       {
         queryAccessor: 'season { season }',
